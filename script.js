265f0e82bbf35747b08b783c402427e03fc4632b
dilip9398 <138967561+dilip9398@users.noreply.github.com>
2024-08-23 19:27:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('header');
+    header.style.animation = 'neon 10s infinite linear';
+});
